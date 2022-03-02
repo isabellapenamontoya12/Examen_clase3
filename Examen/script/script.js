80f8f07b944e0098e2bc3992f979b2d7739abdf6
isabellapenamontoya12 <100798800+isabellapenamontoya12@users.noreply.github.com>
2022-03-02 16:36:19 -0500

    Add files via upload

---
+++ Examen/script/script.js
@@ -0,0 +1,18 @@
+let salario
+let horasT
+const horasE = 30000
+const horas = 20000
+
+function CantHora(params) {
+    horas = Number(prompt('Ingrese cantidad de horas trabajadas'))
+}
+CantHora()
+
+function calcular(salario) {
+    if (horasT >= 1 && horasT <= 40) {
+        salario = CantHora * horas
+    }
+    else if (
+        Extra = horasT - 40)
+}
+
